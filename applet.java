b88568a8fa32e030486105b8ca18715ddb891852
TeeWrath <subroto.2003@gmail.com>
2024-02-20 03:03:18 +0530

    conditional addition of string

--- applet.java
+++ applet.java
@@ -2,7 +2,14 @@ import java.awt.*;
 import java.applet.*;
 
 public class applet extends Applet {
+    String str;
+    public void init(){
+        str = getParameter("string");
+        if (str == null)
+            str = "Java";
+        str = "Hello" + str;
+    }
     public void paint (Graphics g){
-        g.drawString("Hello Java", 10, 100);
+        g.drawString(str, 10, 100);
     }
 }
